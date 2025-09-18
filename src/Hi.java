4e90961782d3efbd51aa34ed832fc8eaa46b6f89
1 <danilsnyiper@gmail.com>
2025-09-18 22:40:36 +0800

    Initial commit

--- src/Hi.java
+++ src/Hi.java
@@ -1,6 +1,7 @@
-void main() {
-    System.out.println("Hello !");
-}//коментарий
-/*многострочный
-кометарий
- */
+public class Hi{
+
+    public static void main (String args[]){
+
+        System.out.println("Hello!");
+    }
+}
